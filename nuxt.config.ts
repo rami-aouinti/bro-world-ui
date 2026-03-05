1e90770f0e6d00f10b4cbe33421e66c1658de364
Rami <rami.aouinti@gmail.com>
2026-03-05 15:33:02 +0100

    chore: load global Material Dashboard styles in Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 export default defineNuxtConfig({
   modules: ["vuetify-nuxt-module"],
-  css: ["vuetify/styles"],
+  css: [
+    "vuetify/styles",
+    "~/app/assets/styles/material-dashboard.scss",
+    "~/app/assets/styles/index.css",
+  ],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
