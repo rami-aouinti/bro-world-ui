783dd46cf5c81263a3b43bcb6ee7e2c333ba44c2
Rami Aouinti <rami.aouinti@gmail.com>
2026-03-07 21:10:04 +0100

    install packages

--- app/components/settings/SettingsLayout.vue
+++ app/components/settings/SettingsLayout.vue
@@ -9,25 +9,21 @@ const isActive = (to: string) => route.path === to
 <template>
   <PlatformSplitLayout>
     <template #sidebar>
-      <UiSectionHeader title="Settings" subtitle="Manage your account preferences" />
-
-      <UiCard rounded="lg" compact>
-        <v-list nav density="comfortable" class="py-0">
-          <v-list-item
+      <v-list nav density="comfortable" class="py-0">
+        <v-list-item
             v-for="item in settingsNavItems"
             :key="item.to"
             :to="item.to"
             :active="isActive(item.to)"
             rounded="lg"
             class="mb-2"
-          >
-            <template #prepend>
-              <v-icon :icon="item.icon" class="me-2" />
-            </template>
-            <v-list-item-title>{{ item.text }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </UiCard>
+        >
+          <template #prepend>
+            <v-icon :icon="item.icon" class="me-2" />
+          </template>
+          <v-list-item-title>{{ item.text }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
     </template>
 
     <template #default>
